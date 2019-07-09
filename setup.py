3e059330650bd4386df8badaebc2982062a259e0
dseidel <dseidel@carbonblack.com>
2019-07-09 13:12:13 -0400

    CB-27598: change package name to cb-stackcollector

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stackcollector',
+    name='cb-stackcollector',
     version='0.2',
     packages=find_packages(),
     install_requires=[
