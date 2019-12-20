746f032dc30baa06628ac0b32e26f972ecd803b1
Jakob <jakob@gambitresearch.com>
2019-12-20 17:34:58 +0000

    small cleanup; add requirments.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cb-stackcollector',
+    name='stackcollector',
     version='0.4',
     packages=find_packages(),
     include_package_data=True,
